a24995cdbde8a1dc5f48e224e0754d490ddf0e23
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2024-11-16 19:21:15 -0800

    add readthedocs

--- langchain_app/t1/t1.py
+++ langchain_app/t1/t1.py
@@ -4,33 +4,34 @@ It loads local PDFs, Python files, and also checks web pages to scrape and consu
 It currently gets responses from Gpt4o, Gemini, and Cluade, though more models could be added.
 """
 
+from bs4 import BeautifulSoup
 from langchain import hub
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.agents import AgentExecutor, create_react_agent
 from langchain_anthropic import ChatAnthropic
 from langchain_chroma import Chroma
+from langchain_community.agent_toolkits.load_tools import load_tools
 from langchain_community.document_loaders import PyPDFLoader, AsyncHtmlLoader
 from langchain_community.document_loaders.generic import GenericLoader
 from langchain_community.document_loaders.parsers import LanguageParser
+from langchain_community.document_transformers import BeautifulSoupTransformer
+from langchain_core.chat_history import InMemoryChatMessageHistory
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.tools import tool
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 import getpass
 import os
-
-llms = [
-    ChatOpenAI(model="gpt-4o"),
-    GoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0),
-    ChatAnthropic(model="claude-3-5-sonnet-latest"),
-]
-
-model_names = ["GPT-4", "Gemini 1.5 Pro", "Claude 3.5 Sonnet"]
-list_len = len(llms)
+import readline
+import subprocess
 
 
 def load_docs(docs):
     """
-    Split text of arg documents and load them into the Chroma vector store
+    Split text of arg documents into chunks and load them into the Chroma vector store
 
     :param docs: List of documents to load and split.
     :type docs: list
@@ -51,7 +52,7 @@ def load_urls(urls):
     load_docs(AsyncHtmlLoader(urls).load())
 
 
-def format_docs(docs):
+def format_docs(docs) -> str:
     """
     Concatenate chunks to include in prompt
 
@@ -67,9 +68,19 @@ def format_docs(docs):
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Google API Key:")
 
+llms = [
+    ChatOpenAI(model="gpt-4o"),
+    GoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0),
+    ChatAnthropic(model="claude-3-5-sonnet-latest"),
+]
+
+model_names = ["GPT-4o", "Gemini 1.5 Pro", "Claude 3.5 Sonnet"]
+list_len = len(llms)
+
 # URL list for scraping
 urls = [
     "https://github.com/GunterMueller/Books-3/blob/master/Design%20Patterns%20Elements%20of%20Reusable%20Object-Oriented%20Software.pdf",
+    "https://rosecityresource.streetroots.org/api/query",
 ]
 
 # Add local pdf file(s)
@@ -79,15 +90,16 @@ pages = []
 for page in loader.lazy_load():
     pages.append(page)
 
-# Add local py files
-pyfiles_path = "."
-pyfiles_loader = GenericLoader.from_filesystem(
-    pyfiles_path,
+# Add local files
+local_path = "."
+local_loader = GenericLoader.from_filesystem(
+    local_path,
     glob="*",
+    # Can select different file suffixes and language types
     suffixes=[".py"],
-    parser=LanguageParser(),
+    parser=LanguageParser(language="python"),
 )
-pyfiles = pyfiles_loader.load()
+local_files = local_loader.load()
 
 # Create a list of vectorstore entries for each model embedding
 vectorstore = list()
@@ -105,7 +117,7 @@ vectorstore.append(
         ),
     )
 )
-# No Claude embedding models readily available; just using Google's
+# No Claude embedding models readily available; instead using Google's
 vectorstore.append(
     Chroma(
         persist_directory="./rag_data/.chromadb/gemini",
@@ -117,8 +129,9 @@ vectorstore.append(
 
 load_urls(urls)
 load_docs(pages)
-load_docs(pyfiles)
+load_docs(local_files)
 
+# Create retriever for accessing & displaying doc info & metadata
 retriever = list()
 for i in range(list_len):
     retriever.append(vectorstore[i].as_retriever())
@@ -134,6 +147,50 @@ for i in range(list_len):
             | StrOutputParser()
         )
     )
+
+memory = InMemoryChatMessageHistory(session_id="test-session")
+
+instructions = """You are an agent that responds to anything the user asks.
+"""
+base_prompt = hub.pull("langchain-ai/react-agent-template")
+prompt = base_prompt.partial(instructions=instructions)
+
+agent_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant."),
+        # First put the history
+        ("placeholder", "{chat_history}"),
+        # Then the new input
+        ("human", "{input}"),
+        # Finally the scratchpad
+        ("placeholder", "{agent_scratchpad}"),
+        instructions,
+    ]
+)
+
+# TODO
+temp = ChatOpenAI(model="gpt-4o")
+
+tools = load_tools(
+    ["serpapi", "llm-math", "wikipedia"],
+    llm=temp,
+    allow_dangerous_tools=False,
+)
+agent = create_react_agent(temp, tools, prompt)
+# NOTE: The verbose setting is great for a look into the model's "thoughts" via stdout
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+agent_with_chat_history = RunnableWithMessageHistory(
+    agent_executor,
+    # This is needed because in most real world scenarios, a session id is needed;
+    # It isn't really used here because we are using a simple in memory ChatMessageHistory
+    lambda session_id: memory,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
+
+config = {"configurable": {"session_id": "test-session"}}
+
 
 print("What kind of questions do you have about the following resources?")
 # Iterate over documents and dump metadata
