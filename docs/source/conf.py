5e2f858ee2273930ed4f4d7619a0a3e2472927a0
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2024-11-29 19:54:11 -0800

    docs: Remove commented-out Napoleon extension and settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -178,7 +178,6 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.githubpages",
     "sphinx.ext.intersphinx",
-    # "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.todo",
     "sphinx_copybutton",
@@ -189,20 +188,6 @@ source_suffix = {
     ".rst": "restructuredtext",
     ".md": "markdown",
 }
-
-# # Napoleon settings
-# napoleon_google_docstring = True
-# napoleon_numpy_docstring = True
-# napoleon_include_init_with_doc = False
-# napoleon_include_private_with_doc = False
-# napoleon_include_special_with_doc = True
-# napoleon_use_admonition_for_examples = False
-# napoleon_use_admonition_for_notes = False
-# napoleon_use_admonition_for_references = False
-# napoleon_use_ivar = False
-# napoleon_use_param = True
-# napoleon_use_rtype = True
-# napoleon_type_aliases = None
 
 # Intersphinx settings
 intersphinx_mapping = {
